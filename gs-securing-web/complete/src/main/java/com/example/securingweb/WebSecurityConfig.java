1db1082d81516d9d3a97ecf0dc3b4d0ef59a9b87
linhne14 <168755243+linhne14@users.noreply.github.com>
2025-09-22 12:50:49 +0700

    fix register

--- gs-securing-web/complete/src/main/java/com/example/securingweb/WebSecurityConfig.java
+++ gs-securing-web/complete/src/main/java/com/example/securingweb/WebSecurityConfig.java
@@ -20,7 +20,7 @@ public class WebSecurityConfig {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
 				.authorizeHttpRequests((requests) -> requests
-						.requestMatchers("/", "/home").permitAll()
+						.requestMatchers("/", "/home", "/register").permitAll()
 						.requestMatchers("/hello").hasRole("USER")
 						.requestMatchers("/admin").hasRole("ADMIN")
 						.anyRequest().authenticated()
